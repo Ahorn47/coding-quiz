b626922f7b057d07d7d72b8e880233490139bbec
Ahorn47 <131202726+Ahorn47@users.noreply.github.com>
2023-06-28 23:57:18 -0500

    added classlist to jscript and onclick

--- script.js
+++ script.js
@@ -1,14 +1,17 @@
-var startbtn = document.querySelector(".start-btn button");
-const infobox = document.querySelector(".info_box");
-const quitbtn = info-box.querySelector(".buttons .quit");
-const continuebtn = document.querySelector(".buttons .restart");
+const startbtn = document.querySelector(".start-btn button");
+const info_box = document.querySelector(".info_box");
+const quitbtn = info_box.querySelector(".buttons .quit");
+const continuebtn = info_box.querySelector(".buttons .restart");
+const quiz_box = document.querySelector(".quiz_box");
 // start button clicked
 startbtn.onclick = function(){
-    infobox.classList.add("activeInfo");
+    info_box.classList.add("activeInfo");
 }
 quitbtn.onclick = function(){
-    infobox.classList.remove("activeInfo");
-}//quit button clicked
-quitbtn.onclick = function(){
-    infobox.classList.add("activeInfo");
+    info_box.classList.remove("activeInfo");
+}
+// continue button clicked
+continuebtn.onclick = function(){
+    info_box.classList.remove("activeInfo");
+    quiz_box.classList.add("activeQuiz");
 }
