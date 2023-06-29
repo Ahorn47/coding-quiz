47bb48bcee44b6476f7c0d8016efb99b316f419d
Ahorn47 <131202726+Ahorn47@users.noreply.github.com>
2023-06-29 16:05:50 -0500

    added js questions to link with js script and added event listerner, onclick, array length, option index

---
+++ questions.js
@@ -0,0 +1,56 @@
+//arrays and questions
+let questions = [
+    { numb: 1,
+    question: "What is an Array?",
+    answer: "An array is commonly referred to as a collection of items stored at memory locations and easily sorted or searched",
+    options: [
+        "An array is a single bracket that contains multiple items",
+        "An array is a collection of items that are rearranged.",
+        "An array is a collection of items that are stored in a single bracket.",
+        "An array is commonly referred to as a collection of items stored at memory locations and easily sorted or searched"
+    ]
+},
+{
+    numb: 2,
+    question: "What is a String?",
+    answer: "A string is a sequence of one or more characters that may consist of letters, numbers, or symbols.",
+    options: [
+        "A string is a sequence of one or more characters that may consist of letters, numbers, or symbols.",
+        "A string is a sequence of just numbers.",
+        "A string is a sequence of symbols.",
+        "A string is a variable that consist of only letters."
+    ]
+},
+{  numb: 3,
+    question: "What is a Boolean?",
+    answer: "A Boolean is a data type that can have one of two values, true or false.",
+    options: [
+        "A Boolean is a data type that is considered only true.",
+        "A Boolean is a data type that can only be false.",
+        "A Boolean is a data type that can have one of two values, true or false.",
+    ]
+},
+{
+    numb: 4,
+    question: "What is a Function?",
+    answer: "A function is a block of code that performs a specific task.",
+    options: [
+        "A function is a block of code that performs a specific task.",
+        "A function is a block of code that performs a series of tasks.",
+        "A function is an attempt to call a value from a function.",
+        "A function is a sequence of numbers letters and symbols that perform a specific object."
+    ]
+},
+{
+    numb: 5,
+    question: "What does HTML stand for?",
+    answer: "Hyper Text Markup Language",
+    options: [
+        "Hyper Text Markup Language",
+        "Hyper Tool Mark Language",
+        "Hyper Text Mark Language",
+        "Just HTML" 
+    ]
+}
+   
+];
--- script.js
+++ script.js
@@ -14,4 +14,50 @@ quitbtn.onclick = function(){
 continuebtn.onclick = function(){
     info_box.classList.remove("activeInfo");
     quiz_box.classList.add("activeQuiz");
+    showQuestions(0)
+}
+let que_count = 0;
+let que_numb = 1;
+
+
+const next_btn = quiz_box.querySelector(".next-btn");
+const bottom_ques_counter = quiz_box.querySelector(".total_que");
+addEventListener("click", function(){
+    if (que_count < questions.length - 1){
+        que_count++;
+        que_numb++;
+        showQuestions(0)
+        queCounter=(que_numb);
+    }else{ console.log("Questions completed");
+    }
+});
+
+// Path: script.js
+function showQuestions(index){
+    const que_text = document.querySelector(".que_text");
+    const option_list = document.querySelector(".option_list");
+    let que_tag = '<span>'+ questions[index].numb + "."+ questions [index].question +'</span>';
+    let option_tag = '<div class="option">'+ questions[index].options[0] +'<span></span></div>'
+    + '<div class="option">'+ questions[index].options[1] +'<span></span></div>'
+    + '<div class="option">'+ questions[index].options[2] +'<span></span></div>'
+    + '<div class="option">'+ questions[index].options[3] +'<span></span></div>';
+    que_text.innerHTML = que_tag;
+    option_list.innerHTML = option_tag;
+    const option = option_list.querySelectorAll(".option");
+    for (let i = 0; i < option_list.length; i++){
+        option_list[i].setAttribute("onclick", "optionSelected(this)");
+    }
+
+
+// Path: script.js
+  function queCounter(index){
+  const bottom_ques_counter = quiz_box.querySelector(".total_que");
+  let totalQuestionCountTag = '<span><p>'+ que_count + '</p>of<p>'+ questions.length +'</p>Questions</span>';
+  bottom_ques_counter.innerHTML = totalQuestionCountTag; 
+}
+}
+function optionSelected(answer){
+    let userAns = answer.textContent;
+    let correctAns = questions[index].answer
+    console.log(userAns);
 }
