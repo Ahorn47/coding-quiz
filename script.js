8bb0c4fb16bb392bc4daf8dd81b420622907bc2b
Ahorn47 <131202726+Ahorn47@users.noreply.github.com>
2023-07-02 17:34:15 -0500

    started over to html added questions easier to code and jsscript used bootstrap for styling and used index,length, loops event listeners and secondsLeft, classes, ids all new

--- script.js
+++ script.js
@@ -1,114 +1,230 @@
-var quiz_box = document.querySelector(".quiz_box");
-var info_box = document.querySelector(".info_box");
-var result_box = document.querySelector(".result_box");
-var start_btn = document.querySelector(".start_btn button");
-var continue_btn = info_box.querySelector(".buttons .continue");
-var exit_btn = info_box.querySelector(".buttons .quit");
-var next_btn = document.querySelector(".next_btn");
-var option_list = document.querySelector(".option_list");
-var timeCount = quiz_box.querySelector(".timer .timer_sec");
-var timeLine = quiz_box.querySelector("header .time_line");
-var timeText = document.querySelector(".timer .timer_left_text");
-
-// if startQuiz button clicked
-  start_btn.onclick = ()=>{
-    info_box.classList.add("activeInfo"); //show info box
-  }
-    
-
-  continue_btn.onclick = ()=>{
-    info_box.classList.remove("activeInfo"); //hide info box
-    quiz_box.classList.add("activeQuiz"); //show quiz box
-    showQuestions(0);
-    queCounter(1); 
-    startTimer(30); 
-    startTimerLine(0); 
-  }
-let que_count = 0;
-let timeValue = 30;
-let que_numb = 1;
-let userScore = 0;
-let counter;
-let counterLine;
-let widthValue = 0;
-
-var restart_quiz = result_box.querySelector(".buttons .restart");
-var quit_quiz = result_box.querySelector(".buttons .quit");
-
-restart_quiz.onclick = ()=>{
-    quiz_box.classList.add("activeQuiz"); 
-    result_box.classList.remove("activeResult"); 
-    timeValue = 30;
-    que_count = 0;
-    que_numb = 1;
-    userScore = 0;
-    widthValue = 0;
-    showQuestions(que_count); 
-    queCounter(que_numb); 
-    clearInterval(counter);
-    clearInterval(counterLine);
-    startTimer(timeValue); 
-    startTimerLine(widthValue); 
-    timeText.textContent = "Time Left"; 
-    next_btn.classList.remove("show"); 
-}
-
-  exit_btn.onclick = ()=>{
-    window.location.reload(); //reload the current window
-  }
-var next_btn = document.querySelector("footer .next_btn");
-var bottom_ques_counter = document.querySelector("footer .total_que");
-
-next_btn.onclick = ()=>{
-if (que_count < questions.length - 1){ 
-  que_count++;
-  que_numb++;
-  showQuestions(que_count);
-  queCounter(que_numb);
-  clearInterval(counter);
-  clearInterval(counterLine);
-  startTimer(timeValue);
-  startTimerLine(widthValue);
-  timeText.textContent = "Time Left";
-  next_btn.classList.remove("show");
-}else{
-  clearInterval(counter);
-  clearInterval(counterLine);
-  showResult();
+let timeEl = document.querySelector("p.time");
+let secondsLeft = 120;
+let scoreEl = document.querySelector("#score");
 
 
-  }
-}
-    function showQuestions(index){
-        var que_text = document.querySelector(".que_text");
+const introEl = document.querySelector("#intro");
 
 
-        let que_tag = '<span>'+ questions[index].numb + ". " + questions[index].question +'</span>';
-        let option_tag = '<div class="option">'+ questions[index].options[0] +'<span></span></div>'
-                        + '<div class="option">'+ questions[index].options[1] +'<span></span></div>'
-                        + '<div class="option">'+ questions[index].options[2] +'<span></span></div>'
-                        + '<div class="option">'+ questions[index].options[3] +'<span></span></div>';
-        que_text.innerHTML = que_tag; 
-        option_list.innerHTML = option_tag;
 
-        
+const questionsEl = document.querySelector("#questions");
 
-        var option = option_list.querySelectorAll(".option");
+let questionEl = document.querySelector("#question");
 
-        for(i=0; i < option.length; i++){
-          option[i].setAttribute("onclick", "optionSelected(this)");
+let questionCount = 0;
+
+const yaynayEl = document.querySelector("#yaynay");
+
+
+const finalEl = document.querySelector("#final");
+
+const highscoresEl = document.querySelector("#highscores");
+
+let scoreListEl = document.querySelector("#score-list");
+
+let scoreList = [];
+
+
+const startBtn = document.querySelector("#start");
+
+const ansBtn = document.querySelectorAll("button.ansBtn")
+
+const ans1Btn = document.querySelector("#answer1");
+
+const ans2Btn = document.querySelector("#answer2");
+
+const ans3Btn = document.querySelector("#answer3");
+
+const ans4Btn = document.querySelector("#answer4");
+
+const submitScrBtn = document.querySelector("#submit-score");
+
+const goBackBtn = document.querySelector("#goback");
+
+const clearScrBtn = document.querySelector("#clearscores");
+
+const viewScrBtn = document.querySelector("#view-scores");
+
+// questions
+const questions = [ // array of objects
+    {
+        // question 0
+        question: "What does HTML stand for?",
+        answers: ["1. just Html", "2. Hyper Text Markup Language", "3. Hyper Tool Mark Language", "4. Hyper Text Processor Language"],
+        correctAnswer: "2"
+    },
+    {
+        // question 1
+        question: "What does a loop do in Javascript?",
+        answers: ["1. loop performs repeated tasks based on a condition", "2. uses multiple tasks to only one condition", "3. a storage for numbers", "4. an item only with square brackets"],
+        correctAnswer: "1"
+    },
+    {
+        // question 2
+        question: "Arrays in Javascript can be used to store ____.",
+        answers: ["1. numbers and strings", "2. other arrays", "3. booleans", "4. all of the above"],
+        correctAnswer: "3"
+    },
+    {
+        // question 3
+        question: "String values must be enclosed within ____ when being assigned to variables.",
+        answers: ["1. commmas", "2. curly brackets", "3. quotes", "4. parentheses"],
+        correctAnswer: "2"
+    },
+    {
+        // question 4
+        question: "what does addEventListener do in JavaScript?",
+        answers: ["1. It makes it easier for one to navigate through a website", "2. Adds many event handlers to one element", "3. lets you onclick to one event only", "4. console.logs the answer"],
+        correctAnswer: "2"
+    }
+];
+
+
+
+
+// timer
+function setTime() {
+    let timerInterval = setInterval(function () {
+        secondsLeft--;
+        timeEl.textContent = `Time:${secondsLeft}s`;
+
+        if (secondsLeft === 0 || questionCount === questions.length) {
+            clearInterval(timerInterval);
+            questionsEl.style.display = "none";
+            scoreEl.textContent = secondsLeft;
         }
+    }, 1000);
+}
+
+// start quiz with timer 
+function startQuiz() {
+    introEl.style.display = "none";
+    questionsEl.style.display = "block";
+    questionCount = 0;
+
+    setTime();
+    setQuestion(questionCount);
+}
+
+
+function setQuestion(id) {
+    if (id < questions.length) {
+        questionEl.textContent = questions[id].question;
+        ans1Btn.textContent = questions[id].answers[0];
+        ans2Btn.textContent = questions[id].answers[1];
+        ans3Btn.textContent = questions[id].answers[2];
+        ans4Btn.textContent = questions[id].answers[3];
+    }
+}
+
+
+function checkAnswer(event) {
+    event.preventDefault();
+
+    
+    let p = document.createElement("p");
+
+    // time out after 1 second
+    setTimeout(function () {
+        p.style.display = 'none';
+    }, 1000);
+
+    // answer checker
+    if (questions[questionCount].correctAnswer === event.target.value) {
+        p.textContent = "Correct!";
+    } else if (questions[questionCount].correctAnswer !== event.target.value) {
+        secondsLeft = secondsLeft - 10;
+        p.textContent = "Wrong!";
     }
 
-    let tickIcon = '<div class="icon tick"><i class="fas fa-check"></i></div>';
-    let crossIcon = '<div class="icon cross"><i class="fas fa-times"></i></div>';
+    // increment so the questions index is increased
+    if (questionCount < questions.length) {
+        questionCount++;
+    }
+    // call setQuestion to bring in next question when any ansBtn is clicked
+    setQuestion(questionCount);
+}
 
-function showResult(){
-  info_box.classList.remove("activeInfo");
-  quiz_box.classList.remove("activeQuiz");
-  result_box.classList.add("activeResult");
-  var scoreText = result_box.querySelector(".score_text");
-  if (userScore > 3){ 
-    let scoreTag = '<span> '
+function addScore(event) {
+    event.preventDefault();
+
+    highscoresEl.style.display = "block";
+
+
+    scoreList.push({score: secondsLeft });
+
+    // sort scores
+    scoreList = scoreList.sort((a, b) => {
+        if (a.score < b.score) {
+          return 1;
+        } else {
+          return -1;
+        }
+      });
+    
+    scoreListEl.innerHTML="";
+    for (let i = 0; i < scoreList.length; i++) {
+        let li = document.createElement("li");
+        li.textContent = `${scoreList[i]}: ${scoreList[i].score}`;
+        scoreListEl.append(li);
+    }
+
+    // Add to local storage
+    storeScores();
+    displayScores();
 }
+
+function storeScores() {
+    localStorage.setItem("scoreList", JSON.stringify(scoreList));
 }
+
+function displayScores() {
+    // Get stored scores from localStorage
+    // Parsing the JSON string to an object
+    let storedScoreList = JSON.parse(localStorage.getItem("scoreList"));
+
+    // If scores were retrieved from localStorage, update the scorelist array to it
+    if (storedScoreList !== null) {
+        scoreList = storedScoreList;
+    }
+}
+
+// clear scores
+function clearScores() {
+    localStorage.clear();
+    scoreListEl.innerHTML="";
+}
+
+// EventListeners
+// Start timer and display first question when click start quiz
+startBtn.addEventListener("click", startQuiz);
+
+// Check answers loop
+ansBtn.forEach(item => {
+    item.addEventListener('click', checkAnswer);
+});
+
+// Add score
+submitScrBtn.addEventListener("click", addScore);
+
+// Go Back Button
+goBackBtn.addEventListener("click", function () {
+    highscoresEl.style.display = "none";
+    introEl.style.display = "block";
+    secondsLeft = 120;
+    timeEl.textContent = `Time:${secondsLeft}s`;
+});
+
+// Clear the scores
+clearScrBtn.addEventListener("click", clearScores);
+
+// View/Hide High Scores Button
+viewScrBtn.addEventListener("click", function () {
+    if (highscoresEl.style.display === "none") {
+        highscoresEl.style.display = "block";
+    } else if (highscoresEl.style.display === "block") {
+        highscoresEl.style.display = "none";
+    } else {
+        return alert("No scores to show.");
+    }
+});
